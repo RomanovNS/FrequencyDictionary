64e89dde9f10580ef8b68d88d0f24e08e98f87a2
RomanovNS <36799997+RomanovNS@users.noreply.github.com>
2020-05-02 18:36:24 +0500

    Multithreading added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class Main{
@@ -27,78 +26,151 @@ public class Main{
     }
 
     public static class FrequencyDictionary{
-        private Map<String, Integer> map = new HashMap<String, Integer>();
+        private Map<String, Integer> dictionary = new HashMap<String, Integer>();
         private int totalCounter = 0;
+        private String wholeText;
 
-        private void FillMap(String text){
-            // сначала переведём всё в нижний регистр чтобы слова не различались из-за регистров
-            String text_LowerCase = text.toLowerCase();
+        public static class DictionaryMaker implements Callable<Map<String, Integer>> {
+            private String localText;
 
-            // теперь заменим все знаки .,<>(){}[];:|\/_-+='"`~!@#№$%^&?* на пробелы  и сразу удалим повторяющиеся пробелы
-            String text_onlyLettersAndNumebers = text_LowerCase.replaceAll("[.,<>(){};:|+-/|_='\"`~!@#№$%^&?—*]"," ")
-                    .replace('[', ' ')
-                    .replace(']', ' ')
-                    .replace('\\', ' ')
-                    .replaceAll("\\s+", " ");
+            private Map<String, Integer> makeDictionary(String text){
+                Map<String, Integer> dictionaryLocal = new HashMap<String, Integer>();
 
-            // а сейчас разделим по пробелам на отдельные слова
-            String[] words = text_onlyLettersAndNumebers.split("\\s+");
+                // сначала переведём всё в нижний регистр чтобы слова не различались из-за регистров
+                String text_LowerCase = text.toLowerCase();
 
-            // а теперь запишем всё в "map", откидывая числа без включения буковок
-            for(String word: words){
-                if (!isDigit(word) && !word.isEmpty()){
-                    if (map.containsKey(word)){
-                        int currentCount = map.get(word);
-                        map.replace(word, currentCount + 1);
+                // теперь заменим все знаки .,<>(){}[];:|\/_-+='"`~!@#№$%^&?* на пробелы  и сразу удалим повторяющиеся пробелы
+                String text_onlyLettersAndNumebers = text_LowerCase.replaceAll("[.,<>(){};:|+-/|_='\"`~!@#№$%^&?—*]"," ")
+                        .replace('[', ' ')
+                        .replace(']', ' ')
+                        .replace('\\', ' ')
+                        .replaceAll("\\s+", " ");
+
+                // а сейчас разделим по пробелам на отдельные слова
+                String[] words = text_onlyLettersAndNumebers.split("\\s+");
+
+                // а теперь запишем всё в dictionary, откидывая числа без включения буковок
+                for(String word: words){
+                    if (!isDigit(word) && !word.isEmpty()){
+                        if (dictionaryLocal.containsKey(word)){
+                            int currentCount = dictionaryLocal.get(word);
+                            dictionaryLocal.replace(word, currentCount + 1);
+                        }
+                        else{
+                            dictionaryLocal.put(word, 1);
+                        }
                     }
-                    else{
-                        map.put(word, 1);
-                    }
-                    totalCounter++;
                 }
+                // а теперь сортируем dictionary по ключу
+                dictionaryLocal = dictionaryLocal.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+                return dictionaryLocal;
             }
-            // а теперь сортируем "map" по ключу
-            map = map.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+            public DictionaryMaker(String textPart){
+                localText = textPart;
+            }
+            @Override
+            public Map<String, Integer> call() throws Exception {
+                return makeDictionary(localText);
+            }
         }
-        public FrequencyDictionary(File file) throws IOException {
-            String text = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
-            FillMap(text);
+
+        void CreateDictionary(int threadsNum){
+            ExecutorService executor = Executors.newCachedThreadPool();
+            List<Future<Map<String, Integer>>> futureLocalDictionaries = new ArrayList<Future<Map<String, Integer>>>();
+
+            // делим текст на равные (ну почти) кусочки так, чтобы не разрывать слова
+            String[] textParts = new String[threadsNum];
+            int textLength = wholeText.length();
+            int substringStart = 0;
+            int substringEnd = 0;
+            int partMidLen = textLength / threadsNum;
+            for(int i = 0; i < threadsNum; i++){
+                // задаём начало подстроки
+                substringStart = substringEnd;
+                // задаём конец подстроки
+                substringEnd = (substringStart + partMidLen < textLength)? substringStart + partMidLen : textLength - 1;
+                while (wholeText.charAt(substringEnd) != ' ' && substringEnd < (textLength - 1))
+                    substringEnd++;
+                // вырезаем кусок текста в подстроку
+                textParts[i] = wholeText.substring(substringStart, substringEnd);
+            }
+
+            // вызываем потоки и объединяем результаты их работы (локальные библиотеки)
+            try{
+                // вызываем потоки
+                for(int i = 0; i < threadsNum; i++){
+                    futureLocalDictionaries.add(executor.submit(new DictionaryMaker(textParts[i])));
+                }
+                // объединяем результаты
+                for(int i = 0; i < threadsNum; i++){
+                    Map<String, Integer> localDictionary = futureLocalDictionaries.get(i).get();
+                    localDictionary.forEach( (k, v) -> dictionary.merge(k, v, (prevCount, count) -> prevCount + count));
+                }
+                // считаем общее количество слов
+                dictionary.forEach( (k, v) -> totalCounter += v);
+            }
+            catch (InterruptedException | ExecutionException exception){
+                System.out.println("Что-то пошло не так при вызове потоков в FrequencyDictionary!");
+                System.exit(666);
+            }
+            finally {
+                executor.shutdown();
+            }
         }
-        public FrequencyDictionary(String text){
-            FillMap(text);
+
+        public FrequencyDictionary(File file, int threads) throws IOException {
+            wholeText = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+            CreateDictionary(threads);
+        }
+        public FrequencyDictionary(String text, int threads){
+            wholeText = text;
+            CreateDictionary(threads);
         }
         public int GetNumberOfAllWords(){
             return totalCounter;
         }
         public int GetNumberOfUniqueWords(){
-            return map.size();
+            return dictionary.size();
         }
         public int GetWordCounter(String word){
-            if (map.containsKey(word.toLowerCase())){
-                return map.get(word);
+            if (dictionary.containsKey(word.toLowerCase())){
+                return dictionary.get(word);
             }
             else return 0;
         }
         public int PrintAllData(){
             System.out.println("Number of words: " + totalCounter + "\nList:\nWord - Counter/Total - Percent");
-            map.forEach( (k, v)->System.out.println(k + " - " + v + "/" + totalCounter + " - " + (double)v * 100.0 / (double)totalCounter) );
+            dictionary.forEach( (k, v)->System.out.println(k + " - " + v + "/" + totalCounter + " - " + (double)v * 100.0 / (double)totalCounter) );
             return 0;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        FrequencyDictionary fd = new FrequencyDictionary(new File("Пища богов.txt"));
+        int threads = 4;
+        long startTime = System.nanoTime();
+        FrequencyDictionary fd = new FrequencyDictionary(new File("test book ''Пища богов''.txt"), threads);
+        long executionTime = System.nanoTime() - startTime;
 
         fd.PrintAllData();
-
         System.out.println();
+        System.out.println("Execution time: " + executionTime / 1000000000.0 + " sec");
+        System.out.println();
+
         System.out.println("Всего слов: " + fd.GetNumberOfAllWords());
         System.out.println("Уникальных слов: " + fd.GetNumberOfUniqueWords());
         System.out.println("Количество слов \"я\": " + fd.GetWordCounter("я") + " (" + fd.GetWordCounter("я") * 100.0 / fd.GetNumberOfAllWords() + "%)");
         System.out.println("Количество слов \"быть\": " + fd.GetWordCounter("быть") + " (" + fd.GetWordCounter("быть") * 100.0 / fd.GetNumberOfAllWords() + "%)");
         System.out.println("Количество слов \"ололо\": " + fd.GetWordCounter("ололо") + " (" + fd.GetWordCounter("ололо") * 100.0 / fd.GetNumberOfAllWords() + "%)");
+
+        //byte[] input = new byte[100];
+        //System.in.read(input);
+        //String str = new String(input);
+        //System.out.println();
+        //System.out.println(str);
+
     }
 }
